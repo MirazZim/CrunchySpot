340c5a90d06985c8f6632abd7e18eadcc9e47cec
MirazZim <miraz.zim.38@gmail.com>
2025-02-22 08:09:07 +0600

    From Awsome react component installed React responsove carousel then created a slider banner

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,35 @@
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import BannerSlider1 from '../../../assets/home/01.jpg'
+import BannerSlider2 from '../../../assets/home/02.jpg'
+import BannerSlider3 from '../../../assets/home/03.png'
+import BannerSlider4 from '../../../assets/home/04.jpg'
+import BannerSlider5 from '../../../assets/home/05.png'
+import BannerSlider6 from '../../../assets/home/06.png'
+
+const Banner = () => {
+    return (
+        <Carousel>
+            <div>
+                <img src={BannerSlider1} alt="Banner Slide 1" />
+            </div>
+            <div>
+                <img src={BannerSlider2} alt="Banner Slide 2" />
+            </div>
+            <div>
+                <img src={BannerSlider3} alt="Banner Slide 3" />
+            </div>
+            <div>
+                <img src={BannerSlider4} alt="Banner Slide 4" />
+            </div>
+            <div>
+                <img src={BannerSlider5} alt="Banner Slide 5" />
+            </div>
+            <div>
+                <img src={BannerSlider6} alt="Banner Slide 6" />
+            </div>
+        </Carousel>
+    );
+};
+
+export default Banner;
