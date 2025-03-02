9081346a8de6dbf00c39d903add7aef763220dc5
MirazZim <miraz.zim.38@gmail.com>
2025-03-02 17:32:16 +0600

    feat: Upload image and add menu item - When someone adds a recipe, the image gets uploaded to ImgBB. - After the image is uploaded, we get a link for it. - Then, we send the recipe name, category, price, and image link to the server. - If everything works, we show a success message saying the item was added.

--- src/Pages/Dashboard/AddItems/AddItems.jsx
+++ src/Pages/Dashboard/AddItems/AddItems.jsx
@@ -2,13 +2,16 @@ import { useForm } from "react-hook-form"
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle"
 import { FaUtensils } from "react-icons/fa"
 import useAxiosOpenForAll from "../../../Hooks/useAxiosOpenForAll";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
 const imageHostingKey = import.meta.env.VITE_IMAGE_HOSTING_KEY;
 const imageHostingApi = `https://api.imgbb.com/1/upload?key=${imageHostingKey}`;
 
 const AddItems = () => {
     const { register, handleSubmit } = useForm()
-    const axiosOpenForAll  = useAxiosOpenForAll();
+    const axiosOpenForAll = useAxiosOpenForAll();
+    const [axiosSecure] = useAxiosSecure();
     const onSubmit = async (data) => {
         console.log(data)
         // image upload to img bb and then get an url
@@ -18,7 +21,31 @@ const AddItems = () => {
                 "content-type": "multipart/form-data",
             },
         })
-        console.log(res.data)
+
+        if (res.data.success) {
+            //now send the menu item data to the server with the image url
+            const image = res.data.data.display_url;
+            const menuItem = {
+                name: data.name,
+                category: data.category,
+                price: parseFloat(data.price),
+                recipe: data.recipe,
+                image
+            }
+            
+            const menuRes = await axiosSecure.post('/menu', menuItem)
+            console.log(menuRes.data)
+            if (menuRes.data.insertedId) {
+                //show success message
+                Swal.fire({
+                    title: ` ${menuItem.name} added to menu`,
+                    text: "Go to menu to complete the order",
+                    icon: "success",
+                    confirmButtonText: "OK"
+                  });
+            }
+        }
+        console.log('with image url',res.data)
     }
     return (
         <div>
@@ -29,17 +56,17 @@ const AddItems = () => {
 
             <div className="max-w-4xl mx-auto p-8 mt-6 bg-white rounded-xl shadow-md border border-gray-200">
                 <h2 className="text-2xl font-bold text-gray-900 mb-6 text-center">Add New Recipe</h2>
-                    {/* name  */}
+                {/* name  */}
                 <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
                     <div>
                         <label htmlFor="name" className="block text-sm font-medium text-gray-700">Recipe name*</label>
                         <input type="text" id="name" className="w-full mt-1 px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-[#16453D] focus:outline-none bg-white" placeholder="Enter recipe name" required {...register("name")} />
                     </div>
-                     {/* category  */}
+                    {/* category  */}
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                         <div>
                             <label htmlFor="category" className="block text-sm font-medium text-gray-700">Category*</label>
-                            <select defaultValue="default" id="category"  className="w-full mt-1 px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-[#16453D] focus:outline-none bg-white" required {...register("category")}>
+                            <select defaultValue="default" id="category" className="w-full mt-1 px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-[#16453D] focus:outline-none bg-white" required {...register("category")}>
                                 <option disabled value="default">Select a category</option>
                                 <option value="pizza">Pizza</option>
                                 <option value="salad">Salad</option>
@@ -50,7 +77,7 @@ const AddItems = () => {
                         </div>
                         <div>
                             <label htmlFor="price" className="block text-sm font-medium text-gray-700">Price*</label>
-                            <input type="number" id="price" className="w-full mt-1 px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-[#16453D] focus:outline-none bg-white" placeholder="Enter price" required {...register("price")} />        
+                            <input type="number" id="price" className="w-full mt-1 px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-[#16453D] focus:outline-none bg-white" placeholder="Enter price" required {...register("price")} />
                         </div>
                     </div>
 
@@ -59,7 +86,7 @@ const AddItems = () => {
                         <label htmlFor="recipe" className="block text-sm font-medium text-gray-700">Recipe Details*</label>
                         <textarea id="recipe" rows="4" className="w-full mt-1 px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-[#16453D] focus:outline-none bg-white" placeholder="Describe the recipe..." required {...register("recipe")} />
                     </div>
-                     {/* image  */}
+                    {/* image  */}
                     <div>
                         <label htmlFor="image" className="block text-sm font-medium text-gray-700">Image</label>
                         <input type="file" id="image" className="w-full mt-1 px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-[#16453D] focus:outline-none bg-white" {...register("image")} />
