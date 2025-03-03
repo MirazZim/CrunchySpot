7bff1a3b96c456ae41a9a1b7c505571219576c6c
MirazZim <miraz.zim.38@gmail.com>
2025-03-03 19:21:14 +0600

    Not Important

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider> {/* ✅ AuthProvider wraps everything */}
       <QueryClientProvider client={queryClient}>
-        <div className="max-w-screen-2xl mx-auto">
+        <div className="max-w-screen-xl mx-auto">
         <RouterProvider router={router} />
         </div>
       </QueryClientProvider>
